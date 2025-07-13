f23f090a6c9273f7b9a0e8e2ef73d737c26b9dbf
Abigail <abigailtrycode@gmail.com>
2025-07-13 14:12:39 +0300

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+Addition = number1 + number2
+subtraction = number2 - number1
+Multiplication = number1 * number2
+
+print("addistion of ", number1 ,"and" ,number2 ,"is" ,Addition)
+print("Subtraction of ", number1, "and", number2, "is", subtraction)
+print("Multiplication of ", number1 ,"and" ,number2 ,"is" ,Multiplication)
