77b7b9c2a8af4e08000605fd4aa137ec34c0dd1d
Abigail <abigailtrycode@gmail.com>
2025-07-23 13:12:01 +0300

    done

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,8 +1,8 @@
-print("Enter the first number: ")
+print("Enter the first number:")
 
 num1 = int(input())
 
-print("Enter the second number: ")
+print("Enter the second number:")
 
 num2 = int(input())
 
