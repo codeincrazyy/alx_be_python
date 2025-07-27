b5815a324b1ec3b54a3ab9e6758335ea3da76fb7
Abigail <abigailtrycode@gmail.com>
2025-07-27 18:07:30 +0300

    done

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
-def perform_operaton(num1, num2, operation):
-    if operation == "add":
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
         return num1 + num2
-    elif operation == "subtrack":
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == "multply":
+    elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Cannot divide by zero"
         return num1 / num2
     else:
-        return "error: invalid operation"
-      
+        return "Error: Invalid operation"
