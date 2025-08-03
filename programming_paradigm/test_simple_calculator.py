ec46858bc18ab520384c1f0c28777569f7b4192c
Abigail <abigailtrycode@gmail.com>
2025-08-03 21:20:11 +0300

    done

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,7 +4,7 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     
-    def setup(self):
+    def setUp(self):
         self.calc=SimpleCalculator()
         
     def test_addition(self):
@@ -16,14 +16,14 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-3, -2),-1)
         self.assertEqual(self.calc.subtract(0, 0), 0)
     def test_multiplication(self):
-        self.assertAlmostEqual(self.calc.multiply(2, 2), 4)
-        self.assertAlmostEqual(self.calc.multiply(-2, -2), -4)
-        self.assertAlmostEqual(self.calc.multiply(0, 0), 0)
+        self.assertEqual(self.calc.multiply(2, 2), 4)
+        self.assertEqual(self.calc.multiply(-2, -2), -4)
+        self.assertEqual(self.calc.multiply(0, 0), 0)
     def test_divination(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-9, -3), 5)
+        self.assertEqual(self.calc.divide(-9, -3), 3)
     def test_DivideByZero(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             self.calc.divide(5, 0)
     if __name__ == '__main__':
         unittest.main()
