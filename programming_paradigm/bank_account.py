2d9d46518ec61ec5841ca806109e0415e0e7183e
Abigail <abigailtrycode@gmail.com>
2025-08-03 20:07:35 +0300

    done

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,6 +10,9 @@ class BankAccount:
         if amount <= self.__balance:
             self.__balance -= amount
             print(f"Withdrew: ${amount}")
+            return True
+        else:
+            return False
     
     def display_balance(self):
         print(f"Current Balance: ${self.__balance:.2f}")
